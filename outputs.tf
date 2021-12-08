b4292a74e33f7d14cb24b1a38fa948f2970482e4
SivaKumar2606 <93194209+SivaKumar2606@users.noreply.github.com>
2021-12-08 16:42:03 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,25 +8,25 @@ output "vm_public_ip_address" {
 # 2. Name of VM
 output "vm_name" {
     description = "Name of the VM"
-    value = azurerm_linux_virtual_machine.myterraformvm.name
+    value = azurerm_virtual_machine.myterraformvm.name
 }
 
 # 3. Output Values - Virtual Machine Resource Group Name
 output "vm_resource_group_name" {
   description = "My Virtual Machine Resource Group Name"
-  value = azurerm_resource_group.mytfrg.name
+  value = data.azurerm_resource_group.mytfrg.name
 
 }
 
 # 4. Output Values - Virtual Machine Location
 output "vm_resource_group_location" {
   description = "My Virtual Machine Location"
-  value = azurerm_resource_group.mytfrg.location
+  value = data.azurerm_resource_group.mytfrg.location
 }
 
 # 5. Output Values - Virtual Machine Network Interface ID
 output "vm_network_interface_ids" {
   description = "My Virtual Machine Network Interface IDs"
-  value = [azurerm_linux_virtual_machine.myterraformvm.network_interface_ids]
+  value = [azurerm_virtual_machine.myterraformvm.network_interface_ids]
 }
 
